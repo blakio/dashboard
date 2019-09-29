6ff878e7474233a41e6380bcd3e46c98d87fa192
Isaiah Harrison <ijh5005@outlook.com>
2019-09-29 10:39:17 -0400

    right panel done

---
+++ src/RightPanel/RightPanelButton/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './RightPanelButton.css';
+
+function RightPanelButton(props) {
+
+  const {
+    title,
+    description,
+    iconClass
+  } = props;
+
+  return (<div className="rightPanelButton flex">
+    <div className="sideButton flex">
+      <i class={iconClass}></i>
+    </div>
+    <div className="sideButtonDescription flex">
+      <div className="sideButtonDescriptionTop sectionHeading">
+        <p>{title}</p>
+      </div>
+      <div className="sideButtonDescriptionBottom sectionHeading">
+        <p>{description}</p>
+      </div>
+    </div>
+  </div>);
+}
+
+export default RightPanelButton;
