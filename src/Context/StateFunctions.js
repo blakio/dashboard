f8c0ad9a1e0d7e12da6f9c44adf660005501bbaf
Isaiah Harrison <ijh5005@outlook.com>
2019-11-08 17:00:34 -0500

    date fixed

--- src/Context/StateFunctions.js
+++ src/Context/StateFunctions.js
@@ -83,8 +83,8 @@ export default {
     Axios.clockOut(employeesId, {
       laborType: isContractor ? "WELDER" : Util.getLaborType(state),
       jobNumber: isContractor ? "35000-222" : Util.getJobNumber(state),
-      date: moment(new Date).format("YYYY/MM/DD"),
-      ...state.selectedItems.employees[0]
+      ...state.selectedItems.employees[0],
+      date: moment(new Date).format("YYYY/MM/DD")
     }, payload);
     return {
       ...state,
