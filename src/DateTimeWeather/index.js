8ac120418c88e54da6a0e3ee6dd8b26e91fc2443
Isaiah Harrison <ijh5005@outlook.com>
2019-11-15 22:36:49 -0500

    date in there

--- src/DateTimeWeather/index.js
+++ src/DateTimeWeather/index.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useContext } from 'react';
 import moment from "moment";
+import axios from "axios";
 
 import TimeSheetContext from "../Context/State";
 import Axios from "../Axios";
@@ -10,20 +11,30 @@ import './DateTimeWeather.css';
 function DateTimeWeather() {
 
   const [time, setTime] = useState(moment().format('MM/DD/YYYY HH:mm:ss'))
+  const [temp, setTemp] = useState("")
 
   useEffect(() => {
     function update() {
-      setTime(moment().format('MM/DD/YYYY HH:mm:ss'))
+      setTime(moment().format('MM/DD/YYYY | HH:mm:ss'));
     }
     setInterval(update, 1000);
+    const apiKey = "1001a1dcc738f2ecade5496fbf796f50";
+    const cityId = "4562144&APPID";
+    const string = `http://api.openweathermap.org/data/2.5/forecast?id=${cityId}=${apiKey}`
+    axios.get(string).then(data => {
+      const K = data.data.list[0].main.temp;
+      const F = (K - 273.15) * (9/5) + 32;
+      setTemp(`${F.toFixed(2)} F`);
+    })
   }, []);
 
   const {
     dispatch
   } = useContext(TimeSheetContext);
 
+
   return (<div id="dateTimeWeather" className="flex">
-    <p>{time}</p>
+    <p>{time} {` | ${temp}`}</p>
   </div>);
 }
 
